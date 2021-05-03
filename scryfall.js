ff49a194a87612f0ffdfe0765f3bd6d39571a05b
James Stevenson <mooseling@hotmail.com>
2021-05-03 15:44:53 +0100

    Add getCardByName function

---
+++ scryfall.js
@@ -0,0 +1,25 @@
+const scryfall = (function(){
+  return {getCardByName};
+
+
+  async function getCardByName(cardName) {
+    const endpoint = 'cards/named?exact=' + cardName;
+
+    const response = await scryfall.api.get(endpoint);
+    return response.json();
+  }
+}());
+
+
+scryfall.api = (function(){
+  const baseUrl = 'https://api.scryfall.com';
+
+  return {get};
+
+
+  async function get(endpoint) {
+    const url = `${baseUrl}/${endpoint}`;
+
+    return fetch(url);
+  }
+}());
