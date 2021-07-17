96c1b33f32da850ba25f448b509c8624d1547516
CallanHP <callanhp@live.com>
2021-07-17 21:36:24 +1000

    Swapped to fuzzy searching

--- src/CardService.js
+++ src/CardService.js
@@ -29,7 +29,7 @@ RiptideLab.CardService = (function(){
 
   // When a card is not found, Scryfall returns a json response and a 404 status
   async function getCardFromExternalService(cardName) {
-    const endpoint = 'cards/named?exact=' + encodeURIComponent(cardName);
+    const endpoint = 'cards/named?fuzzy=' + encodeURIComponent(cardName);
     let card;
 
     try {
