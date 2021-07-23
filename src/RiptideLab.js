2c6058583294807abc30e985c0937a77ae70b4c9
James Stevenson <mooseling@hotmail.com>
2021-07-23 13:51:26 +0100

    [Tooltip] Give tooltip a border

--- src/RiptideLab.js
+++ src/RiptideLab.js
@@ -1,5 +1,5 @@
 /* exported RiptideLab */
 
 const RiptideLab = {
-  tooltipContentStyle: 'display:inline-block;background-color:#f7f7f7;padding:8pt;border-color:#ccc'
+  tooltipContentStyle: 'display:inline-block;background-color:#f7f7f7;padding:8pt;border:1px solid #ccc'
 };
