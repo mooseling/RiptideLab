1124b105be12f66b2015cde08f7877d8a82bad0c
James Stevenson <mooseling@hotmail.com>
2021-08-02 17:07:51 +0100

    [Embedding] Add document ready listener

--- src/embedded-images.js
+++ src/embedded-images.js
@@ -1,15 +1,25 @@
-document.addEventListener(
-  'load',
-  async function(event) {
-    const img = event.target;
-    const classList = img?.classList;
-    if (!classList || !classList.contains('RiptideLab--unloaded-card-image'))
-      return;
-
-    const cardName = img.dataset.cardName;
+(function() {
+  async function replaceWithViewer(element) {
+    const cardName = element.dataset.cardName;
     const card = await RiptideLab.Card(cardName);
     const viewer = RiptideLab.CardViewer(card, {embedded: true});
-    img.replaceWith(viewer);
-  },
-  true // Catch load events during the capture phase, since img loads do not bubble
-);
+    element.replaceWith(viewer);
+  }
+
+  document.addEventListener(
+    'load',
+    async function(event) {
+      const img = event.target;
+      const classList = img?.classList;
+      if (classList && classList.contains('RiptideLab--unloaded-card-image'))
+        replaceWithViewer(img);
+    },
+    true // Catch load events during the capture phase, since img loads do not bubble
+  );
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const elementsToLoad = document.getElementsByClassName('RiptideLab--unloaded-card-image');
+    for (const element of elementsToLoad)
+      replaceWithViewer(element);
+  });
+})();
