036e36fe688569bdb630744da7b148c2cf8168eb
James Stevenson <mooseling@hotmail.com>
2021-08-17 10:41:29 +0100

    [Test] Add timestamper

---
+++ test/timestamper.js
@@ -0,0 +1,21 @@
+const stamps = [];
+
+function stamp() {
+  stamps.push(Date.now());
+}
+
+function getSmallestGap() {
+  let smallestGap = Infinity;
+  for (let index = 1; index < stamps.length; index++) {
+    const thisStamp = stamps[index];
+    const lastStamp = stamps[index - 1];
+    if (thisStamp !== undefined && lastStamp !== undefined) {
+      const gap = thisStamp - lastStamp;
+      if (gap < smallestGap)
+        smallestGap = gap;
+    }
+  }
+  return smallestGap;
+}
+
+module.exports = {stamp, getSmallestGap};
